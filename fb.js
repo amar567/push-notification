8645cfa0bfb9a38356ecf7a89ec17273aba6939f
Amardeep <amarthescientist@gmail.com>
2023-12-21 16:31:38 +0530

    first commit

---
+++ fb.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app"
+import { getMessaging, getToken } from "firebase/messaging";
+
+const firebaseConfig = {}
+
+// Initialize Firebase 
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Cloud Messaging and get a reference to the service 
+const messaging = getMessaging(app);
+
+getToken(messaging, { vapidKey: '<YOUR_PUBLIC_VAPID_KEY_HERE>' }).then((currentToken) => {
+    if (currentToken) { 
+        // Send the token to your server and update the UI if necessary
+    } else {
+        // Show permission request UI 
+        console.log('No registration token available. Request permission to generate one.');
+    }
+}
+).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+});
