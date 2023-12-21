85f2342dfb9d1189404c80f42ba03c5258340df6
Amardeep <amarthescientist@gmail.com>
2023-12-21 20:05:54 +0530

    .

--- sw.js
+++ sw.js
@@ -1,7 +1,28 @@
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
+self.addEventListener('push', function (event) {
+    // const data = event.data.json();
 
-    event.waitUntil(
-        self.registration.showNotification(data.title, data.options)
-    );
+    // event.waitUntil(
+    //     self.registration.showNotification(data.title, data.options)
+    // );
 });
+
+if (navigator.setAppBadge) {
+    // Display the number of unread messages.
+    navigator.setAppBadge(numberOfUnreadMessages);
+}
+
+// Listen to "push" events in the service worker.
+self.addEventListener("push", (event) => {
+    // Extract the unread count from the push message data.
+    const message = event.data.json();
+    const unreadCount = message.unreadCount;
+  
+    // Set or clear the badge.
+    if (navigator.setAppBadge) {
+      if (unreadCount && unreadCount > 0) {
+        navigator.setAppBadge(unreadCount);
+      } else {
+        navigator.clearAppBadge();
+      }
+    }
+  });
